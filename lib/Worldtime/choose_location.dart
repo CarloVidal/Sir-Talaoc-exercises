add75f6d619511145231889cbb7b97dfa5d4282b
CarloVidal <jcarlovidal1@gmail.com>
2025-07-07 09:39:46 +0800

    commit muna

---
+++ lib/Worldtime/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose Location'),
+      ),
+      body: Center(
+        child: Text('Location Selection Screen'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-
 import 'pages/listitem.dart';
 import 'pages/add_drink.dart';
-
+import 'Worldtime/home.dart';
+import 'Worldtime/choose_location.dart';
 void main() {
   runApp(
     MaterialApp(
-      routes: {
-        '/': (context) => const ListItem(),
+      routes: {       
+        '/home': (context) => Home(),
         '/list': (context) =>  ListItem(),
         '/add': (context) => const DrinkKinds(),
+        
+       //world time routes
+        '/choose_location':(context) =>ChooseLocation(),
+      
       },
     ),
   );
